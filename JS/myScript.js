7fd7889423093dc6ecae6d73a3664fbe3899a52b
Lee10 <leembuyisa22@gmail.com>
2021-02-11 12:05:17 +0200

    Update

---
+++ JS/myScript.js
@@ -0,0 +1,12 @@
+//Back Button
+function Back(){
+	window.history.back();
+}
+function goForward(){
+	window.history.forward();
+}
+function pre(){
+	window.history.back();
+}
+
+
---
+++ JS/nav.js
@@ -0,0 +1,40 @@
+let hamburger = document.querySelector(".hamburger-menu");
+let links = document.querySelector("ul");
+
+let link = document.querySelectorAll("ul li");
+
+hamburger.addEventListener("click", () =>{
+	links.classList.toggle("active");
+
+});
+
+
+document.addEventListener('DOMContentLoaded', () => createNav());
+
+let navBar= [
+{title: 'Blog', link: 'Blog.html'},
+{title: 'About', link: 'About.html'},
+
+//{title: 'Creative Projects', link: 'Creative.html'},
+
+
+];
+
+
+let createNav = () => {
+let nav = document.querySelector('nav');
+ let ul = document.createElement('ul');
+  
+ for (let item of navBar){
+
+ 	let li = document.createElement('li');
+ 	let a = document.createElement('a');
+ 	a.innerText =item.title;
+ 	a.href =item.link;
+ 	li.appendChild(a);
+ 	ul.appendChild(li);
+ }
+
+ nav.appendChild(ul);
+};
+
